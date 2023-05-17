465345f57fce9f4d97b9f67b832e5c910567e910
VladimirIvancha <Vladimir.Ivancha@mail.ru>
2023-05-17 15:41:41 +0300

    feat: add sidebar btns action

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RecipeItemsInitialData } from '../../utils/initialData'
 import NavBarStickyTop from '../NavBarStickyTop/NavBarStickyTop'
 import RecipeItem from '../RecipeItem/RecipeItem'
 import HomeSideBar from './HomeSideBar/HomeSideBar'
 
 function Home() {
+
+  const [recipeType, setRecipeType] = useState('all');
+
   return (
     <section className="home">
       <NavBarStickyTop />
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 col-sm-4">
-            <HomeSideBar />
+            <HomeSideBar setRecipeType={setRecipeType} />
           </div>
           <div className="col-12 col-sm-8">
             {
               RecipeItemsInitialData.map(item => (
-                <RecipeItem key={item.id} item={item} />
+                recipeType === 'all' ?
+                  <RecipeItem key={item.id} item={item} />
+                  :
+                  item.type === recipeType &&
+                  <RecipeItem key={item.id} item={item} />
               ))
             }
           </div>
--- src/components/Home/HomeSideBar/HomeSideBar.jsx
+++ src/components/Home/HomeSideBar/HomeSideBar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { homeSideBarInitialData } from '../../../utils/initialData'
 import HomeSideBarItem from './HomeSideBarItem/HomeSideBarItem'
 
-function HomeSideBar() {
-  const [recipeType, setRecipeType] = useState('');
+function HomeSideBar({ setRecipeType }) {
+
 
   return (
     <section className='homeSideBar'>
@@ -11,8 +11,8 @@ function HomeSideBar() {
         <div className="mainNavigationSection">
           <div className="section_title">Почитать</div>
           {
-            homeSideBarInitialData.map(item => (
-              <HomeSideBarItem key={item.id} item={item} setRecipeType={setRecipeType} />
+            homeSideBarInitialData.map((item) => (
+                <HomeSideBarItem key={item.id} item={item} setRecipeType={setRecipeType} />
             ))
           }
         </div>
