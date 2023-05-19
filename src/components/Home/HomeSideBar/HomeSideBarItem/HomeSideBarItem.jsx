f8ce8ed7555c5d17e39718f3235c8d7689099617
VladimirIvancha <Vladimir.Ivancha@mail.ru>
2023-05-19 13:12:30 +0300

    feat: add scrollToTop hook, 4 new recipes

--- src/components/Home/HomeSideBar/HomeSideBarItem/HomeSideBarItem.jsx
+++ src/components/Home/HomeSideBar/HomeSideBarItem/HomeSideBarItem.jsx
@@ -4,6 +4,7 @@ function HomeSideBarItem({ item, setRecipeType }) {
 
   function sidebarItemClick() {
     setRecipeType(item.type);
+    window.scrollTo(0, 0);
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import "../src/pages/index.css";
 import App from './components/App/App';
+import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
